88c5170f4a8ae49ea8280d27c77fc967d10b52e8
= <chad.sheridan@cysec.ca>
2025-08-10 20:33:29 -0400

    Change logging level to INFO for improved log clarity

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ logging.basicConfig(
     filename='app.log',
     filemode='a',
     format='%(asctime)s %(levelname)s: %(message)s',
-    level=logging.DEBUG
+    level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
